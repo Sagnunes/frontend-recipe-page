79c2a647bcab990539945ca1be4ec4cf89eb937b
Sérgio Nunes <sagnunes@gmail.com>
2024-02-15 16:11:00 +0000

    finish project

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,30 @@
-/** @type {import('tailwindcss').Config} */
+/** @type {import("tailwindcss").Config} */
 module.exports = {
-  content: [],
+  content: ["./src/**/*.{html,js}"],
   theme: {
     extend: {},
+    screens: {
+      xs: "375px",
+      sm: "640px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1280px",
+      "2xl": "1440px",
+    },
+    colors: {
+      White: "hsl(0, 0%, 100%)",
+      Nutmeg: "hsl(14, 45%, 36%)",
+      DarkRaspberry: "hsl(332, 51%, 32%)",
+      RoseWhite: "hsl(330, 100%, 98%)",
+      Eggshell: "hsl(30, 54%, 90%)",
+      LightGrey: "hsl(30, 18%, 87%)",
+      WengeBrown: "hsl(30, 10%, 34%)",
+      DarkCharcoal: "hsl(24, 5%, 18%)",
+    },
+    fontFamily: {
+      outfit: ["Outfit"],
+      youngSerif: ["Young Serif"],
+    },
   },
   plugins: [],
-}
-
+};
